e5a29dcc9dfa876aca2b2fb52c6430f910f5a1e0
naieemsupto <naieem.mahmud@selise.ch>
2017-01-16 10:52:42 +0600

    edit modal er click er date change er prblm ta solve

--- js/controller.js
+++ js/controller.js
@@ -4,7 +4,7 @@
  * @param  {[type]} $ionicModal      [description]
  * @param  {Array}  ionicDatePicker
  */
- angular.module('ionicApp').controller('AppCtrl', function($rootScope, $scope, $ionicModal, $ionicPopup, $cordovaToast, $cordovaLocalNotification, $ionicSideMenuDelegate) {
+angular.module('ionicApp').controller('AppCtrl', function($rootScope, $scope, $ionicModal, $ionicPopup, $cordovaToast, $cordovaLocalNotification, $ionicSideMenuDelegate) {
     $scope.duty = [];
     $scope.mnth = [];
     $scope.search = [];
@@ -16,7 +16,7 @@
      * @param  {[type]} localStorage.getItem("duties") [description]
      * @return {[type]}                                [description]
      */
-     if (localStorage.getItem("duties") == null) {
+    if (localStorage.getItem("duties") == null) {
         $scope.duties = [];
         localStorage.setItem("duties", angular.toJson($scope.duties));
 
@@ -29,7 +29,7 @@
      * @param  {[type]} modal) {                       $scope.modal [description]
      * @return {[type]}        [description]
      */
-     $ionicModal.fromTemplateUrl('add.html', {
+    $ionicModal.fromTemplateUrl('add.html', {
         scope: $scope
     }).then(function(modal) {
         $scope.modal = modal;
@@ -63,29 +63,28 @@
      * [add_entry adding new duty list]
      * @param {[type]} event [description]
      */
-     $scope.add_modal = function() {
+    $scope.add_modal = function() {
         //event.preventDefault();
         //$scope.dte = '';
         $scope.show_add_true = true;
         $scope.modal.show();
     }
     $scope.add_entry = function(event) {
-        event.preventDefault();
-        if ($scope.duty.place == undefined || $scope.duty.place == "" || $scope.duty.dte == undefined || $scope.duty.dte == "" || $scope.duty.amount == undefined || $scope.duty.amount == "" || $scope.duty.provider == undefined || $scope.duty.provider == "" || $scope.duty.schedule == undefined || $scope.duty.schedule == "") {
-            $cordovaToast.showShortBottom('Please Provide All Information To Add Schedule')
-            .then(
-                function(success) {},
-                function(error) {
-                    alert(error);
-                }
-                );
-        } else {
-            $scope.show_add_true = false;
-            var temp_date = new Date($scope.duty.dte);
-            var paid = $scope.duty.paid ? true : false;
-            var mnth = month[temp_date.getMonth()];
-            var dt = temp_date.getDate();
-            var yr = temp_date.getFullYear();
+            event.preventDefault();
+            if ($scope.duty.place == undefined || $scope.duty.place == "" || $scope.duty.dte == undefined || $scope.duty.dte == "" || $scope.duty.amount == undefined || $scope.duty.amount == "" || $scope.duty.provider == undefined || $scope.duty.provider == "" || $scope.duty.schedule == undefined || $scope.duty.schedule == "") {
+                $cordovaToast.showShortBottom('Please Provide All Information To Add Schedule')
+                    .then(
+                        function(success) {},
+                        function(error) {
+                            alert(error);
+                        }
+                    );
+            } else {
+                var temp_date = new Date($scope.duty.dte);
+                var paid = $scope.duty.paid ? true : false;
+                var mnth = month[temp_date.getMonth()];
+                var dt = temp_date.getDate();
+                var yr = temp_date.getFullYear();
                 //var rnd = randomString(5);
                 var rnd = Math.floor((Math.random() * 1000) + 1);
 
@@ -117,11 +116,11 @@
                 var t = angular.fromJson(localStorage.getItem('duties'));
                 console.log(t);
                 $cordovaToast.showShortBottom('New Schedule Added')
-                .then(
-                    function(success) {},
-                    function(error) {
-                        alert(error);
-                    }
+                    .then(
+                        function(success) {},
+                        function(error) {
+                            alert(error);
+                        }
                     );
                 $scope.modal.hide();
             }
@@ -131,7 +130,7 @@
          * @param  {[int]} dt [ index of the array ]
          * @return {[type]}    [description]
          */
-         $scope.delete = function(dt) {
+    $scope.delete = function(dt) {
             var dcsn = confirm("Do you want to delete!");
             if (dcsn == true) {
                 for (var i = 0; i < $scope.duties.length; i++) {
@@ -148,12 +147,15 @@
          * @param  {[int]} dt [ index of the array]
          * @return {[type]}    [description]
          */
-         $scope.edit = function(dt) {
-            $scope.show_add_true = true;
+    $scope.edit = function(dt) {
             for (var i = 0; i < $scope.duties.length; i++) {
                 if ($scope.duties[i].random == dt) {
                     $scope.edit_data = [];
-                    $scope.edit_data = $scope.duties[i];
+                    $scope.edit_data.place = $scope.duties[i].place;
+                    $scope.edit_data.schedule = $scope.duties[i].schedule;
+                    $scope.edit_data.provider = $scope.duties[i].provider;
+                    $scope.edit_data.paid = $scope.duties[i].paid;
+                    $scope.edit_data.amount = $scope.duties[i].amount;
                     $scope.edit_data.dte = new Date($scope.duties[i].fulldate);
                     $scope.edit_data.index = i;
                     $scope.edit_modal.show();
@@ -167,60 +169,60 @@
          * @param  {[type]} index [description]
          * @return {[type]}       [description]
          */
-         $scope.edit_entry = function(index) {
-            console.log($scope.duties[index]);
-            if ($scope.edit_data.place == undefined || $scope.edit_data.place == "" || $scope.edit_data.dte == undefined || $scope.edit_data.dte == "" || $scope.edit_data.amount == undefined || $scope.edit_data.amount == "" || $scope.edit_data.provider == undefined || $scope.edit_data.provider == "" || $scope.edit_data.schedule == undefined || $scope.edit_data.schedule == "") {
-                $cordovaToast.showShortBottom('Please Provide All Information To Add Schedule')
+    $scope.edit_entry = function(index) {
+        console.log($scope.duties[index]);
+        if ($scope.edit_data.place == undefined || $scope.edit_data.place == "" || $scope.edit_data.dte == undefined || $scope.edit_data.dte == "" || $scope.edit_data.amount == undefined || $scope.edit_data.amount == "" || $scope.edit_data.provider == undefined || $scope.edit_data.provider == "" || $scope.edit_data.schedule == undefined || $scope.edit_data.schedule == "") {
+            $cordovaToast.showShortBottom('Please Provide All Information To Add Schedule')
                 .then(
                     function(success) {},
                     function(error) {
                         alert(error);
                     }
-                    );
-            } else {
-                $scope.show_add_true = false;
-                console.log($scope.edit_data);
-                var temp_date = new Date($scope.edit_data.dte);
-                var mnth = month[temp_date.getMonth()];
-                var dt = temp_date.getDate();
-                var yr = temp_date.getFullYear();
-                $scope.duties[index].place = $scope.edit_data.place;
-                $scope.duties[index].fulldate = $scope.edit_data.dte;
-                $scope.duties[index].amount = $scope.edit_data.amount;
-                $scope.duties[index].provider = $scope.edit_data.provider;
-                $scope.duties[index].schedule = $scope.edit_data.schedule;
-                $scope.duties[index].month = mnth;
-                $scope.duties[index].date = dt;
-                $scope.duties[index].year = yr;
-                $scope.duties[index].paid = $scope.edit_data.paid ? true : false;
+                );
+        } else {
+            $scope.show_add_true = false;
+            console.log($scope.edit_data);
+            var temp_date = new Date($scope.edit_data.dte);
+            var mnth = month[temp_date.getMonth()];
+            var dt = temp_date.getDate();
+            var yr = temp_date.getFullYear();
+            $scope.duties[index].place = $scope.edit_data.place;
+            $scope.duties[index].fulldate = $scope.edit_data.dte;
+            $scope.duties[index].amount = $scope.edit_data.amount;
+            $scope.duties[index].provider = $scope.edit_data.provider;
+            $scope.duties[index].schedule = $scope.edit_data.schedule;
+            $scope.duties[index].month = mnth;
+            $scope.duties[index].date = dt;
+            $scope.duties[index].year = yr;
+            $scope.duties[index].paid = $scope.edit_data.paid ? true : false;
 
-                localStorage.setItem("duties", angular.toJson($scope.duties));
-                $cordovaLocalNotification.schedule({
-                    id: $scope.edit_data.random,
-                    title: 'Duty Notification',
-                    text: 'You have a duty today in: ' + $scope.edit_data.place + ' Schedule: ' + $scope.edit_data.schedule,
-                    firstAt: $scope.edit_data.dte
-                }).then(function(result) {
+            localStorage.setItem("duties", angular.toJson($scope.duties));
+            $cordovaLocalNotification.schedule({
+                id: $scope.edit_data.random,
+                title: 'Duty Notification',
+                text: 'You have a duty today in: ' + $scope.edit_data.place + ' Schedule: ' + $scope.edit_data.schedule,
+                firstAt: $scope.edit_data.dte
+            }).then(function(result) {
                 //alert(result);
             });
-                $scope.edit_data = [];
-                var t = angular.fromJson(localStorage.getItem('duties'));
-                console.log(t);
-                $cordovaToast.showShortBottom('Schedule Edited')
+            $scope.edit_data = [];
+            var t = angular.fromJson(localStorage.getItem('duties'));
+            console.log(t);
+            $cordovaToast.showShortBottom('Schedule Edited')
                 .then(
                     function(success) {},
                     function(error) {
                         alert(error);
                     }
-                    );
-                $scope.edit_modal.hide();
-            }
+                );
+            $scope.edit_modal.hide();
         }
+    }
 
     /**
      * @param  Secret key of the schedule
      */
-     $scope.show = function(dt) {
+    $scope.show = function(dt) {
         $scope.show_add_true = true;
         for (var i = 0; i < $scope.duties.length; i++) {
             if ($scope.duties[i].random == dt) {
@@ -246,8 +248,7 @@
     /**
      * All Modal Hide Actions After Cancel button Press
      */
-     $scope.add_modal_hide = function() {
-        //$scope.show_add_true = false;
+    $scope.add_modal_hide = function() {
         $scope.modal.hide();
     }
 
@@ -257,7 +258,6 @@
     }
 
     $scope.show_modal_hide = function() {
-        //$scope.show_add_true = false;
         $scope.show_modal.hide();
     }
 
@@ -265,7 +265,7 @@
     /**
      * On click event to the schedule notification
      */
-     $rootScope.$on('$cordovaLocalNotification:click',
+    $rootScope.$on('$cordovaLocalNotification:click',
         function(event, notification, state) {
             // alert(notification.id);
             // var alertPopup = $ionicPopup.alert({
@@ -279,4 +279,4 @@
             $scope.show(notification.id);
         });
 
- });
+});
