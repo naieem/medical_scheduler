da919ac855eeba0ae4ecd6342034da189ec9d006
naieemsupto <naieem.mahmud@selise.ch>
2017-01-18 19:52:52 +0600

    images are moved to new folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,22 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 var del = require('del');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('copy', function() {
-  return gulp.src(['./**/*', '!./node_modules/**/*','!./debug.log','!./description.txt'])
-  .pipe(gulp.dest('../medical_app_release'))
+    return gulp.src(['./**/*', '!./node_modules/**/*', '!./debug.log', '!./description.txt'])
+        .pipe(gulp.dest('../medical_app_release'))
 });
 
-gulp.task('clean', function(){
-     return del('../medical_app_release.zip', {force:true});
+gulp.task('clean', function() {
+    return del('../medical_app_release.zip', { force: true });
 });
-// gulp.task('copy_js', function() {
-//   return gulp.src('./js/**/*')
-//   .pipe(gulp.dest('../medical_app_release/js'))
-// });
-// gulp.task('copy_css', function() {
-//   return gulp.src('./css/**/*')
-//   .pipe(gulp.dest('../medical_app_release/css'))
-// });
-// gulp.task('copy_fonts', function() {
-//   return gulp.src('./fonts/**/*')
-//   .pipe(gulp.dest('../medical_app_release/fonts'))
-// });
-// gulp.task('copy_others', function() {
-//   return gulp.src(['./icon.png','./config.xml','./gulpfile.js','./images.jpg','./index.html','./package.json','./splash.jpg'])
-//   .pipe(gulp.dest('../medical_app_release'))
-// });
+
+gulp.task('optimize', function() {
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist'))
+});
 
 gulp.task('zip', function() {
     return gulp.src('../medical_app_release/**/*')
@@ -33,5 +24,5 @@ gulp.task('zip', function() {
         .pipe(gulp.dest('../'));
 });
 
-gulp.task('default',['clean','copy']);
-gulp.watch('./**/*', ['default']); 
+gulp.task('default', ['clean', 'copy', 'optimize']);
+gulp.watch('./**/*', ['default']);
